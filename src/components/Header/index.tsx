4b619619882b281e328f580b2e6fe933dfdfc704
ChristophSiegenthaler <csi@gmx.ch>
2020-06-09 23:29:03 +0200

    Logo changes and redirect fix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ import { Text } from 'rebass'
 
 import styled from 'styled-components'
 
-import Logo from '../../assets/svg/logo.svg'
+import Logo from '../../assets/svg/unicorn.svg'
 import LogoDark from '../../assets/svg/logo_white.svg'
 import Wordmark from '../../assets/svg/wordmark.svg'
 import WordmarkDark from '../../assets/svg/wordmark_white.svg'
@@ -157,7 +157,7 @@ export default function Header() {
         <HeaderElement>
           <Title>
             <UniIcon id="link" to="/">
-              <img src={isDark ? LogoDark : Logo} alt="logo" />
+              <img src={Logo} alt="logo" height="80px" />
             </UniIcon>
             {!isMobile && (
               <TitleText>
